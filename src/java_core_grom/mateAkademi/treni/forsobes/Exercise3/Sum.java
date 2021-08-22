72951022ebc1fc89b37e4dd450d9f6f399c90ccf
toniname <drednoyta1@ukr.net>
2021-08-22 10:41:15 +0300

    init commit

--- src/java_core_grom/mateAkademi/treni/forsobes/Exercise3/Sum.java
+++ src/java_core_grom/mateAkademi/treni/forsobes/Exercise3/Sum.java
@@ -1,18 +1,20 @@
 package java_core_grom.mateAkademi.treni.forsobes.Exercise3;
-
+/*Find the sum of the digits in the number 100! (i.e. 100 factorial)
+        {Correct answer: 648}*/
 import java.math.BigInteger;
 
 public class Sum {
     public long sumOfTheDigits(int digit) {
 
-        BigInteger fact = BigInteger.ONE;
-        for (int i = 1; i <= digit; i++) {
-            fact = fact.multiply(BigInteger.valueOf(i));
+        BigInteger value = BigInteger.ONE; // declaring a large number and assigning a constant 1
+        for (int i = 1; i <= digit; i++) { //iteration  over a given (digit) number of cells
+            value = value.multiply(BigInteger.valueOf(i)); // multiplication of one and selected values from above
         }
         long sum = 0;
-        while (fact.compareTo(BigInteger.ZERO) > 0) {
-             sum += fact.mod(BigInteger.TEN).longValue();
-            fact = fact.divide(BigInteger.TEN);
+
+        while (value.compareTo(BigInteger.ZERO) > 0) {  // zero check
+            sum += value.mod(BigInteger.TEN).longValue(); // sum non-negative and converts this BigInteger to a long.
+            value = value.divide(BigInteger.TEN); // Returns and assigns  value the result of division (this / val).
         }
         return sum;
     }
