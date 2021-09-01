9d0d33dd01543bb362c562771fd747835dea0f1e
toniname <drednoyta1@ukr.net>
2021-09-01 09:50:48 +0300

    init commit

--- src/java_core_grom/mateAkademi/treni/forsobes/Exercise2/Demo.java
+++ src/java_core_grom/mateAkademi/treni/forsobes/Exercise2/Demo.java
@@ -1,4 +1,33 @@
 package java_core_grom.mateAkademi.treni.forsobes.Exercise2;
 
 public class Demo {
+
+    public static void main(String[] args) {
+
+        Graph graph = new Graph();
+
+        graph.addVertex(Name.GDANSK);
+        graph.addVertex(Name.BYDGOSZCZ);
+        graph.addVertex(Name.TORUN);
+        graph.addVertex(Name.WARSZAWA);
+
+        graph.addEdge(0,1, 1);
+        graph.addEdge(1,2, 1);
+        graph.addEdge(0,2, 3);
+        graph.addEdge(2,3, 4);
+        graph.addEdge(1,3, 1);
+
+
+        //graph.passInDeep(0, 1);
+        graph.passInWidth(0);
+
+
+        int[][] city = {{ 0, 1, 3, 5 },
+                        { 1, 0, 1, 4 },
+                        { 3, 1, 0, 1 },
+                        { 5, 4, 1, 0 }};
+
+
+    }
+
 }
