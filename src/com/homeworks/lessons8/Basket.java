e198d647d88a8e7a873121ae04ebd2b935649cd4
toniname <drednoyta1@ukr.net>
2021-10-02 19:43:00 +0300

    init commit

---
+++ src/com/homeworks/lessons8/Basket.java
@@ -0,0 +1,31 @@
+package com.homeworks.lessons8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Basket extends Shop implements Action{
+
+
+    public Basket(List<Object> product) {
+        super(product);
+    }
+
+    @Override
+    public void addProductInBasket() {
+    }
+
+    @Override
+    public void deleteProductInBasket() {
+
+    }
+
+    @Override
+    public void addSimilarProductInBasket() {
+
+    }
+
+    @Override
+    public void findProduct() {
+
+    }
+}
