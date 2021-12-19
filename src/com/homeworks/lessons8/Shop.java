32be33c627feb71c254a72a9998360e08d7dc85f
toniname <drednoyta1@ukr.net>
2021-12-19 22:56:26 +0200

    init commit

--- src/com/homeworks/lessons8/Shop.java
+++ src/com/homeworks/lessons8/Shop.java
@@ -1,7 +1,5 @@
 package com.homeworks.lessons8;
 
-import java.util.List;
-
 /*Создать приложение интернет - магазина(консольное). Приложение должен выполнять следующие операции:
 1) добавление товара(название, производитель, дата производства и т.д.).
 2) добавление сопутствующих товаров.
@@ -10,14 +8,97 @@ import java.util.List;
 5) выбор товара по определенным критериям и после выводить сопутствующие товары.
 Учесть, что каждого товара определенное количество в магазине.
 Примеры товаров: мобильные телефоны, телевизоры и т.д.*/
-public abstract class Shop {
-    private final List<Object>Product;
+public class Shop {
 
-    public Shop(List<Object> product) {
-        Product = product;
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String productName;
+
+    private String phone;
+
+    private String email;
+
+    public Shop() {
     }
 
-    public List<Object> getProduct() {
-        return Product;
+    public Shop(String firstName, String lastName, String productName, String phone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.productName = productName;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Shop(Long id, String firstName, String lastName, String productName, String phone, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.productName = productName;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    @Override
+    public String toString() {
+        return "Shop{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", productName='" + productName + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
